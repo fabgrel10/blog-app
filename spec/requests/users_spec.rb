1f0b8c81a107b1504583ca1775307c2e733f0bcf
Fabricio Garcia <fabgrel10@gmail.com>
2022-05-30 19:28:45 -0500

    🧪 Add tests cases for Users controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,44 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Users', type: :request do
+  describe 'GET #index' do
+    before(:each) { get users_path }
+
+    it 'returns a 200 status code or ok response' do
+      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'index' template" do
+      expect(response).to render_template(:index)
+    end
+
+    it 'does not render a different template' do
+      expect(response).to_not render_template('/show')
+    end
+
+    it 'includes a placeholder' do
+      expect(response.body).to include('Users#index')
+    end
+  end
+
+  describe 'GET #show' do
+    before(:each) { get '/users/:id' }
+
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'show' template" do
+      expect(response).to render_template(:show)
+    end
+
+    it 'does not render a different template' do
+      expect(response).to_not render_template('/index')
+    end
+
+    it 'includes a placeholder' do
+      expect(response.body).to include('Users#show')
+    end
   end
 end
