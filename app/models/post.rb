e9f436ed67bab8edcd5158cc448d570b873f7452
Fabricio Garcia <fabgrel10@gmail.com>
2022-05-31 18:07:10 -0500

    🔨 Create associations and validations in Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,26 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  after_save :increment_posts_counter
+  after_destroy :decrement_posts_counter
+
+  private
+
+  def increment_posts_counter
+    author.increment!(:posts_counter)
+  end
+
+  def decrement_posts_counter
+    author.decrement!(:posts_counter)
+  end
+
+  def recent_comments
+    comments.last(5)
+  end
 end
