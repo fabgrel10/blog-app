51b9866a939cda4456e1eaccd161aa7daf389879
Fabricio Garcia <fabgrel10@gmail.com>
2022-06-01 20:48:30 -0500

    ✨ Create Users#index view
    - Modify controller's Users#index method
    - Create user_avatar helper method in application_helper
    - Add initial style to application.html.erb layout
    - Add initial style to Users#index view
    - Create _user partials in views/partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def user_avatar(user)
+    if user.photo.nil?
+      image_tag user.photo, width: '100'
+    else
+      image_tag 'user_photo.png', width: '100'
+    end
+  end
 end
