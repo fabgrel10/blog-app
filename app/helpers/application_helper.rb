e776da768d33787d199c6bd6c628d304c081378e
Fabricio Garcia <fabgrel10@gmail.com>
2022-06-11 17:28:41 -0500

    🐛 Fix user_avatar helper
    Method now shows a default picture if none is provided, otherwise
    displays the current user photo but only if it is avatar size.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def user_avatar(user)
     if user.photo.nil?
-      image_tag user.photo, width: '100'
-    else
       image_tag 'user_photo.png', width: '100'
+    else
+      image_tag(user.photo, alt: user.name, width: '100')
     end
   end
 
