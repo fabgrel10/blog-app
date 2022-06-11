eaaa333eb3a7f25b355319f60bfaef7ccc7cecbf
Fabricio Garcia <fabgrel10@gmail.com>
2022-06-11 14:24:46 -0500

    🧪 Create Post model specs

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,62 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) do
+    @author = User.create(name: 'Test User', photo: 'https://example.com/test.jpg', bio: 'I am a test user ',
+                          posts_counter: 0)
+
+    @post = Post.create(
+      author: @author,
+      title: 'Test Post',
+      text: 'I am a test post',
+      comments_counter: 5,
+      likes_counter: 10
+    )
+  end
+
+  context 'post creation' do
+    it 'to be present' do
+      @post.title = nil
+      expect(@post).to_not be_valid
+    end
+
+    it 'to not have more than 250 characters' do
+      @post.title = 'a' * 251
+      expect(@post).to_not be_valid
+    end
+  end
+
+  context '.comments_counter' do
+    it 'to be present' do
+      @post.comments_counter = nil
+      expect(@post).to_not be_valid
+    end
+
+    it 'to be an integer' do
+      @post.comments_counter = 1.5
+      expect(@post).to_not be_valid
+    end
+
+    it 'to be greater than or equal to zero' do
+      @post.comments_counter = -1
+      expect(@post).to_not be_valid
+    end
+  end
+
+  context '.likes_counter' do
+    it 'to be present' do
+      @post.likes_counter = nil
+      expect(@post).to_not be_valid
+    end
+
+    it 'to be an integer' do
+      @post.likes_counter = 1.5
+      expect(@post).to_not be_valid
+    end
+
+    it 'to be greater than or equal to zero' do
+      @post.likes_counter = -1
+      expect(@post).to_not be_valid
+    end
+  end
 end
