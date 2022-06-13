d721eee1d613ad8c57ac6bb3f2cc8cd6d37dfce1
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-06-13 19:16:31 -0300

    ✨ Add functionality to create a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'sign_up' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    get '/sign_out', to: 'users/sessions#destroy'
+  end
+
   root 'users#index'
 
   resources :users, only: [:index, :show] do
