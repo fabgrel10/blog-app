55e0d4e6b0c5d3d601926c664df025663e7d0ed5
Fabricio Garcia <fabgrel10@gmail.com>
2022-06-16 16:14:35 -0500

    🔨  Create posts endpoint to list all posts for a user
    - In Post#index all posts are shown for that user
    - In User#show a specific post is shown for that user

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::PostsController < ApplicationController
+  def index
+    user = User.find(params[:user_id])
+    render json: user.posts, status: :ok
+  rescue StandardError => e
+    render json: { error: e }, status: :not_found
+  end
+
+  def show
+    user = User.find(params[:user_id])
+    post = user.posts.find(params[:id])
+    render json: post, status: :ok
+  rescue StandardError => e
+    render json: { error: e }, status: :not_found
+  end
+end
