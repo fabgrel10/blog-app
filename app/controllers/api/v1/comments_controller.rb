4ff4b727cd86a8d63c78001dcc7c7431dcb6d92d
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-06-16 19:26:37 -0300

    �  Create Comments#create endpoint to add a comment

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -15,4 +15,20 @@ class Api::V1::CommentsController < ApplicationController
   rescue StandardError => e
     render json: { error: e }, status: :not_found
   end
+
+  def create
+    user = User.find(params[:user_id])
+    post = user.posts.find(params[:post_id])
+    comment = post.comments.build(comment_params)
+    comment.author_id = current_user.id
+    render json: comment, status: :ok if comment.save
+  rescue StandardError => e
+    render json: { error: e }, status: :error
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
