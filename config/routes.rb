7140bc12e1aa707686262b52c0d921aabba02782
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-06-17 17:05:19 -0300

    ✨ Create JWT service and RegistrationsController

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      devise_for :users, as: 'api', controllers: { sessions: 'api/sessions', registrations: 'api/registrations' }
+      devise_for :users, as: 'api', controllers: { sessions: 'api/sessions', registrations: 'api/v1/registrations' }
 
       resources :users, only: %i[index show] do
         resources :posts, only: %i[index show] do
